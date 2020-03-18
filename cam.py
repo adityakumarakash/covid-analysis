f1c6e8d27cc2e92ba1246b0ca5a40e0fd153b2f9
Aditya Kumar Akash <adityakumarakash@gmail.com>
2020-03-18 17:34:17 -0500

    Adding initial look at the chestxray data

---
+++ cam.py
@@ -0,0 +1,83 @@
+import torch
+import numpy as np
+import copy
+import matplotlib.cm as mpl_color_map
+import pdb
+from torchvision import transforms
+from PIL import Image
+import skimage
+from skimage import util
+
+class Hook():
+    def __init__(self, module, backward=False):
+        if backward==False:
+            self.hook = module.register_forward_hook(self.hook_fn)
+        else:
+            self.hook = module.register_backward_hook(self.hook_fn)
+    
+    def hook_fn(self, module, input, output):
+        self.input = input
+        self.output = output
+    
+    def close(self):
+        self.hook.remove()
+
+def threshold_activations(activation):
+    #activation[activation<230] = 70
+    return activation
+        
+def apply_colormap_on_image(org_im, activation, colormap_name='jet'):
+    """
+    Apply heatmap on image
+    Args:
+    org_img (PIL img): Original image
+    activation_map (numpy arr): Activation map (grayscale) 0-255
+    colormap_name (str): Name of the colormap
+    """
+    # Get colormap
+    color_map = mpl_color_map.get_cmap(colormap_name)
+    no_trans_heatmap = color_map(threshold_activations(activation))
+    
+    # Change alpha channel in colormap to make sure original image is displayed
+    heatmap = copy.copy(no_trans_heatmap)
+    heatmap[:, :, 3] = 0.4
+    heatmap = Image.fromarray((heatmap*255).astype(np.uint8))
+    
+    # Apply heatmap on iamge
+    heatmap_on_image = Image.new("RGBA", org_im.size)
+    heatmap_on_image = Image.alpha_composite(heatmap_on_image, org_im.convert('RGBA'))
+    heatmap_on_image = Image.alpha_composite(heatmap_on_image, heatmap)
+    return heatmap_on_image
+        
+        
+def get_cam_images(model, last_conv_layer, images):
+    # Add hook to get last conv layer's features
+    hook = Hook(model._modules.get(last_conv_layer))
+    model.eval()
+
+    # Get the weights for the last global average pooled layer
+    params = list(model.parameters())
+    weight_softmax = params[-2]
+    
+    pred = model(images)
+    
+    def get_cam_activations(conv_features, weights, class_idx):
+        # conv_features = batch * channels * w * h
+        # weights = classes * channels
+        # class_idx = class for which cam is to be generated
+        _, _, w, h = conv_features.shape
+        class_weights = weights[class_idx]  # dim = channels
+        cam_features = torch.sum(conv_features * class_weights.view(1, -1, 1, 1), 1)
+        cam_features -= cam_features.view(-1, w*h).min(1)[0].view(-1, 1, 1)
+        cam_features /= cam_features.view(-1, w*h).max(1)[0].view(-1, 1, 1)
+        return np.uint8(255 * cam_features.cpu().detach().numpy())
+    
+    corona_idx = 2
+    cam_activations = get_cam_activations(hook.output, weight_softmax, corona_idx)
+    cam_activations = [util.img_as_ubyte(skimage.transform.resize(inp, images[0].shape[-2:])) for inp in cam_activations]
+    hook.close()
+    transform = transforms.ToPILImage()
+    pil_images = [transform(img) for img in images.cpu()]
+    color_heatmaps = [apply_colormap_on_image(img, act) for img, act in zip(pil_images, cam_activations)]
+    return color_heatmaps
+        
---
+++ datasets.py
@@ -0,0 +1,165 @@
+# From https://github.com/mlmed/torchxrayvision/blob/master/torchxrayvision/datasets.py
+from PIL import Image
+from os.path import join
+from skimage.io import imread, imsave
+from torch import nn
+from torch.nn.modules.linear import Linear
+from torch.utils.data import Dataset
+from torchvision import transforms
+from tqdm import tqdm
+import numpy as np
+import os,sys,os.path
+import pandas as pd
+import pickle
+import skimage
+import glob
+import collections
+import pprint
+import torch
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms.functional as TF
+import skimage.transform
+import warnings
+import tarfile
+
+default_pathologies = [  'Atelectasis',
+                 'Consolidation',
+                 'Infiltration',
+                 'Pneumothorax',
+                 'Edema',
+                 'Emphysema',
+                 'Fibrosis',
+                 'Effusion',
+                 'Pneumonia',
+                 'Pleural_Thickening',
+                 'Cardiomegaly',
+                 'Nodule',
+                 'Mass',
+                 'Hernia',
+                 'Lung Lesion',
+                 'Fracture',
+                 'Lung Opacity',
+                 'Enlarged Cardiomediastinum'
+                ]
+
+thispath = os.path.dirname(os.path.realpath(__file__))
+
+def normalize(sample, maxval):
+    """Scales images to be roughly [-1024 1024]."""
+    sample = (2 * (sample.astype(np.float32) / maxval) - 1.) * 1024
+    #sample = sample / np.std(sample)
+    return sample
+
+def relabel_dataset(pathologies, dataset):
+    """
+    Reorder, remove, or add (nans) to a dataset's labels.
+    Use this to align with the output of a network.
+    """
+    will_drop = set(dataset.pathologies).difference(pathologies)
+    if will_drop != set():
+        print("{} will be dropped".format(will_drop))
+    new_labels = []
+    dataset.pathologies = list(dataset.pathologies)
+    for pathology in pathologies:
+        if pathology in dataset.pathologies:
+            pathology_idx = dataset.pathologies.index(pathology)
+            new_labels.append(dataset.labels[:,pathology_idx])
+        else:
+            print("{} doesn't exist. Adding nans instead.".format(pathology))
+            values = np.empty(dataset.labels.shape[0])
+            values.fill(np.nan)
+            new_labels.append(values)
+    new_labels = np.asarray(new_labels).T
+    
+    dataset.labels = new_labels
+    dataset.pathologies = pathologies
+
+class Dataset():
+    def __init__(self):
+        pass
+    def totals(self):
+        counts = [dict(collections.Counter(items[~np.isnan(items)]).most_common()) for items in self.labels.T]
+        return dict(zip(self.pathologies,counts))
+    def check_paths_exist(self):
+        if not os.path.isdir(self.imgpath):
+            raise Exception("imgpath must be a directory")
+        if not os.path.isfile(self.csvpath):
+            raise Exception("csvpath must be a file")
+
+
+class COVID19_Dataset(Dataset):
+
+    def __init__(self, 
+                 imgpath=os.path.join(thispath, "covid-chestxray-dataset", "images"), 
+                 csvpath=os.path.join(thispath, "covid-chestxray-dataset", "metadata.csv"), 
+                 transform=None, 
+                 data_aug=None, 
+                 nrows=None, 
+                 seed=0,
+                 pure_labels=False, 
+                 unique_patients=True):
+
+        super(COVID19_Dataset, self).__init__()
+        np.random.seed(seed)  # Reset the seed so all runs are the same.
+        self.imgpath = imgpath
+        self.transform = transform
+        self.data_aug = data_aug
+        
+        # defined here to make the code easier to read
+        pneumonias = ["COVID-19", "SARS", "MERS", "ARDS", "Streptococcus"]
+        
+        self.pathologies = ["Pneumonia","Viral Pneumonia", "Bacterial Pneumonia", "No Finding"] + pneumonias
+        self.pathologies = sorted(self.pathologies)
+
+        mapping = dict()
+        mapping["Pneumonia"] = pneumonias
+        mapping["Viral Pneumonia"] = ["COVID-19", "SARS", "MERS"]
+        mapping["Bacterial Pneumonia"] = ["Streptococcus"]
+        
+        # Load data
+        self.csvpath = csvpath
+        self.csv = pd.read_csv(self.csvpath, nrows=nrows)
+        self.MAXVAL = 255  # Range [0 255]
+
+        # Keep only the PA view.
+        idx_pa = self.csv["view"] == "PA"
+        self.csv = self.csv[idx_pa]
+        
+        self.labels = []
+        for pathology in self.pathologies:
+            mask = self.csv["finding"].str.contains(pathology)
+            if pathology in mapping:
+                for syn in mapping[pathology]:
+                    #print("mapping", syn)
+                    mask |= self.csv["finding"].str.contains(syn)
+            self.labels.append(mask.values)
+        self.labels = np.asarray(self.labels).T
+        self.labels = self.labels.astype(np.float32)
+
+    def __repr__(self):
+        pprint.pprint(self.totals())
+        return self.__class__.__name__ + " num_samples={}".format(len(self))
+    
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        imgid = self.csv['filename'].iloc[idx]
+        img_path = os.path.join(self.imgpath, imgid)
+        img = Image.open(img_path)     
+                               
+        if self.transform is not None:
+            img = self.transform(img)
+        labels = self.labels[idx]
+        labels = torch.from_numpy(labels)
+            
+        return {"PA":img, "lab":labels, "idx":idx}
+    
+    
+class ToPILImage(object):
+    def __init__(self):
+        self.to_pil = transforms.ToPILImage(mode="F")
+
+    def __call__(self, x):
+        return(self.to_pil(x[0]))
